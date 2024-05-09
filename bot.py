65dce3c9abcdb96f417b6d29b759e909ddc8485b
Ghrts <159820216+Ghrts@users.noreply.github.com>
2024-05-09 21:18:34 +0300

    bot.py(v1)

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def nasilimm(ctx):
+    await ctx.send("FaÇaSıN kArDeŞiM bEnİm DiKkAt Et KeSmE bİzİ hA")
+    
+@bot.command()
+async def bye(ctx):
+    await ctx.send("baysssss :3 :3 :3")
+       
+@bot.command()
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+bot.run()
